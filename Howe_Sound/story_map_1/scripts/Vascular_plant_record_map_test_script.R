cca8d00cdb34bf4c329c4a2c2feb894ebb5191d3
Antranig Basman <antranig.basman@colorado.edu>
2022-12-29 00:14:56 +0000

    First working version with peculiar application of "here"

--- Howe_Sound/story_map_1/scripts/Vascular_plant_record_map_test_script.R
+++ Howe_Sound/story_map_1/scripts/Vascular_plant_record_map_test_script.R
@@ -5,15 +5,18 @@
 # script using the R Markdown 'scrolly-telling' framework we've developed, but
 # it fails to knit...
 
+root <- "Howe_Sound/story_map_1/"
+
+# This line is not actually necessary
+# here::i_am(file.path(root, "scripts/Vascular_plant_record_map_test_script.R"))
+
 library(here)
 library(sf)
 library(leaflet)
 library(dplyr)
 library(raster)
 
-# Set relative paths using 'here'
-
-here::i_am("R-Markdown-AHSBR_Map_1.Rmd")
+setwd(here(root));
 
 # Source dependencies
 
