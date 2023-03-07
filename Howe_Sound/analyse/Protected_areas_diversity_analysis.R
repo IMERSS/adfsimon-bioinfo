01f4644952184ebb1f4a6f27fd99c4b58df637f5
chlorophilia <andrewsimon@MacBook-Air.lan>
2023-03-07 01:22:12 -0800

    reran gridded history analysis to correct error in how plant species richness was summed within grid cells; updated protected areas analysis script

--- Howe_Sound/analyse/Protected_areas_diversity_analysis.R
+++ Howe_Sound/analyse/Protected_areas_diversity_analysis.R
@@ -34,7 +34,10 @@ native.plants <- native.plants %>% drop_na(yearRecorded)
 
 protected.areas <- st_read("spatial_data/vectors/Protected_Areas")
 
+protected.areas$native.plant.diversity <- native.plant.diversity
+
 names(protected.areas)[names(protected.areas) == 'NAME_E'] <- 'protectedArea'
+names(protected.areas)[names(protected.areas) == 'TYPE_E'] <- 'protectedAreaType'
 
 # Create CRS object
 
@@ -52,23 +55,33 @@ plants.x.protected.areas <- st_intersection(native.plants, protected.areas)
 
 # Summarize native plant diversity in protected areas
 
-protected.native.plant.diversity <- length(unique(plants.x.protected.areas$scientificName))
+ protected.areas$protected.native.plant.diversity <- length(unique(plants.x.protected.areas$scientificName))
 
 # Sum species richness by protected area
 
 plants.x.protected.areas$count <- 1
 
-matrix <- ecodist::crosstab(plants.x.protected.areas$protectedArea, plants.x.protected.areas$scientificName, plants.x.protected.areas$count)
+protected.area.matrix <- ecodist::crosstab(plants.x.protected.areas$protectedArea, plants.x.protected.areas$scientificName, plants.x.protected.areas$count)
 
-matrix[matrix > 1] <- 1
+protected.area.matrix[protected.area.matrix > 1] <- 1
 
-matrix$richness <- rowSums(matrix)
+protected.area.matrix$richness <- rowSums(protected.area.matrix)
 
-matrix$protectedArea <- row.names(matrix)
+protected.area.matrix$protectedArea <- row.names(protected.area.matrix)
+
+# Sum species richness by protected area type
+
+type.matrix <- ecodist::crosstab(plants.x.protected.areas$protectedAreaType, plants.x.protected.areas$scientificName, plants.x.protected.areas$count)
+
+type.matrix[type.matrix > 1] <- 1
+
+type.matrix$richness <- rowSums(type.matrix)
+
+type.matrix$protectedAreaType <- row.names(type.matrix)
 
 # Assign richness values to grid
 
-protected.areas$richness <- matrix$richness[match(unlist(protected.areas$protectedArea), matrix$protectedArea)]
+protected.areas$protected.area.richness <- protected.area.matrix$richness[match(unlist(protected.areas$protectedArea), protected.area.matrix$protectedArea)]
 
-
+protected.areas$protected.area.type.richness <- type.matrix$richness[match(unlist(protected.areas$protectedAreaType), type.matrix$protectedAreaType)]
 
