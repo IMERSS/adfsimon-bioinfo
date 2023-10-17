4fcfd8eb02e63dbdf2f294def240041e5d2e8469
chlorophilia <andrewsimon@MacBook-Air.lan>
2023-10-16 23:47:45 -0700

    generated script for curation of Galiano Island terrestrial arthropod diversity
    prepared curated summary of Galiano Island terrestrial arthropod diversity

--- Biodiversity_Galiano/2022-2023/parse_iNat_records/parse_iNat_records.R
+++ Biodiversity_Galiano/2022-2023/parse_iNat_records/parse_iNat_records.R
@@ -26,11 +26,24 @@ iNat.obs$Date.observed <- substr(iNat.obs$Date.observed,1,10)
 
 # Parse taxa
 
+# Terrestrial Arthropods
+
+unique(Terrestrial.arthropods.obs$Class)
+
+Terrestrial.arthropods.1.obs <- iNat.obs %>% filter(Class == 'Insecta' | Class == 'Arachnida' | Class == 'Entognatha' | Class == 'Diplopoda' | Class == 'Chilopoda' | Class == 'Ostracoda')
+Terrestrial.arthropods.2.obs <- iNat.obs %>% filter(Order == 'Isopoda')
+Terrestrial.arthropods.2.obs<- subset(Terrestrial.arthropods.2.obs, Taxon.name != 'Bopyroides hippolytes' & Taxon.name != 'Gnorimosphaeroma oregonense' & Taxon.name != 'Pentidotea resecata' & Taxon.name != 'Pentidotea wosnesenskii')
+
+Terrestrial.arthropods.obs <- rbind(Terrestrial.arthropods.1.obs, Terrestrial.arthropods.2.obs)
+
+
 # Tracheophyta
 
 Tracheophyta.obs <- iNat.obs %>% filter(Phylum == 'Tracheophyta')
 
 # Export taxonomically parsed catalogs
 
+
+write.csv(Terrestrial.arthropods.obs, "outputs/iNat_obs_terrestrial_arthropods.csv")
 write.csv(Tracheophyta.obs, "outputs/iNat_obs_Tracheophyta.csv")
 
