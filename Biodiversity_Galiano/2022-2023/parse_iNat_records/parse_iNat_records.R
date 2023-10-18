c1c56bacf703bfb88bb885e065bfe13dacd2dcbf
chlorophilia <andrewsimon@MacBook-Air.lan>
2023-10-17 22:28:46 -0700

    updated BioGaliano source data readme parsed iNat data for Galiano Island's terrestrial mammals generated summary R script for Galiano Island's terrestrial mammals reviewed and exported summary of Galiano Island's terrestrial mammals

--- Biodiversity_Galiano/2022-2023/parse_iNat_records/parse_iNat_records.R
+++ Biodiversity_Galiano/2022-2023/parse_iNat_records/parse_iNat_records.R
@@ -28,13 +28,17 @@ iNat.obs$Date.observed <- substr(iNat.obs$Date.observed,1,10)
 
 # Terrestrial Arthropods
 
-unique(Terrestrial.arthropods.obs$Class)
-
 Terrestrial.arthropods.1.obs <- iNat.obs %>% filter(Class == 'Insecta' | Class == 'Arachnida' | Class == 'Entognatha' | Class == 'Diplopoda' | Class == 'Chilopoda' | Class == 'Ostracoda')
 Terrestrial.arthropods.2.obs <- iNat.obs %>% filter(Order == 'Isopoda')
 Terrestrial.arthropods.2.obs<- subset(Terrestrial.arthropods.2.obs, Taxon.name != 'Bopyroides hippolytes' & Taxon.name != 'Gnorimosphaeroma oregonense' & Taxon.name != 'Pentidotea resecata' & Taxon.name != 'Pentidotea wosnesenskii')
 
 Terrestrial.arthropods.obs <- rbind(Terrestrial.arthropods.1.obs, Terrestrial.arthropods.2.obs)
+
+# Terrestrial Mammals
+
+Terrestrial.mammals.obs <- iNat.obs %>% filter(Class == 'Mammalia')
+
+Terrestrial.mammals.obs <- subset(Terrestrial.mammals.obs, Infraorder != 'Cetacea' & Taxon.name != 'Neogale vison' & Superfamily != 'Phocoidea' & Subfamily != 'Lutrinae')
 
 
 # Tracheophyta
@@ -45,5 +49,6 @@ Tracheophyta.obs <- iNat.obs %>% filter(Phylum == 'Tracheophyta')
 
 
 write.csv(Terrestrial.arthropods.obs, "outputs/iNat_obs_terrestrial_arthropods.csv")
+write.csv(Terrestrial.mammals.obs, "outputs/iNat_obs_terrestrial_mammals.csv")
 write.csv(Tracheophyta.obs, "outputs/iNat_obs_Tracheophyta.csv")
 
