e036ac93349c877edb31e17e6d2732da1fbb6ff2
chlorophilia <andrewsimon@MacBook-Air-474.local>
2024-11-21 14:40:57 -0700

    updated phytoplankton taxon x sample matrix

--- Biodiversity_Galiano/consolidate_records/Plantae_et_Chromista/macroalgae_zooplankton_and_phytoplankton/phytoplankton_review/Generate_phytoplankton_taxon_x_sample_matrix.R
+++ Biodiversity_Galiano/consolidate_records/Plantae_et_Chromista/macroalgae_zooplankton_and_phytoplankton/phytoplankton_review/Generate_phytoplankton_taxon_x_sample_matrix.R
@@ -37,5 +37,5 @@ colnames(table)[1:5] <- c("iNaturalist", "BioBlitz 2023", "Zostera 2020", "Miner
 table <- table[, c(1, 3, 4, 5, 2)]
 
 write.csv(phytoplankton.records, "/Users/andrewsimon/GitHub/adfsimon-bioinfo/Biodiversity_Galiano/consolidate_records/Plantae_et_Chromista/macroalgae_zooplankton_and_phytoplankton/phytoplankton_review/Galiano_Island_phytoplankton_records.csv", row.names = FALSE)
-write.csv(table, "/Users/andrewsimon/GitHub/adfsimon-bioinfo/Biodiversity_Galiano/consolidate_records/Plantae_et_Chromista/macroalgae_zooplankton_and_phytoplankton/phytoplankton_review/Galiano_Island_phytoplankton_taxon_x_sample_matrix.csv", row.names = FALSE)
+write.csv(table, "/Users/andrewsimon/GitHub/adfsimon-bioinfo/Biodiversity_Galiano/consolidate_records/Plantae_et_Chromista/macroalgae_zooplankton_and_phytoplankton/phytoplankton_review/Galiano_Island_phytoplankton_taxon_x_sample_matrix.csv")
 
