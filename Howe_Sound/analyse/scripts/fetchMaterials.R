56a6f897a6f1dd996ab5fb209b5185e048509c29
chlorophilia <andrewsimon@MacBook-Air-474.lan>
2025-01-22 09:29:05 -0700

    fetchMaterials.R script for Howe Sound vascular plant data suspended for now

--- Howe_Sound/analyse/scripts/fetchMaterials.R
+++ Howe_Sound/analyse/scripts/fetchMaterials.R
@@ -1,10 +1,11 @@
 setwd(paste0(dirname(rstudioapi::getActiveDocumentContext()$path), "/.."))
 
-# When working from GitHub, this script draws bulky biodiversity data from Google Drive
+# When working from GitHub, this script draws bulky biodiversity spatial data from Google Drive
+
+# NOTE: THIS SCRIPT IS NOT CURRENTLY IN USE AS WE HAVE DECIDED TO AVOID USING BULKY SPATIAL DATA
 
 source("scripts/utils.R")
 
-# From location IMERSS Research Projects/Community Research Projects/2022-2025 - Howe Sound Biodiversity/Spatial_Data/
-# https://drive.google.com/drive/folders/1i9-LHxIsIqpXGH8bKAavJXAjZLNOJkOs
-downloadGdriveFolder("1i9-LHxIsIqpXGH8bKAavJXAjZLNOJkOs", ".../Spatial_Data", FALSE)
-
+# From location IMERSS Research Projects/Community Research Projects/2022-2025 - Howe Sound Biodiversity/spatial_data/vectors/Gridded_confirmed_records_2025
+# https://drive.google.com/drive/folders/1WH8eY1K8ZWLLZux-YsSe6mfdFdyEUs1N
+# downloadGdriveFolder("1WH8eY1K8ZWLLZux-YsSe6mfdFdyEUs1N", "spatial_data/vectors/", FALSE)
