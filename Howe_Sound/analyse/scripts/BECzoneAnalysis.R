02ac687c42c7de4cae05f6e6ecf44b35b6e4243e
chlorophilia <andrewsimon@MacBook-Air-474.lan>
2025-01-25 18:27:15 -0700

    saving changes to BECzoneAnalysis for Howe Sound vascular plants

--- Howe_Sound/analyse/scripts/BECzoneAnalysis.R
+++ Howe_Sound/analyse/scripts/BECzoneAnalysis.R
@@ -79,42 +79,37 @@ BEC <- BEC %>%
   summarize(geometry = st_union(geometry), .groups = "drop")
 
 # Intersect plant occurrences x BEC Zones
+# Note: intersecting BEC occurrences x BEC zones using the reverse order to retain
+# polygon geometries is extremely slow. We shall bypass the need to extract these geometries
+# at this stage and assign taxa to BEC Zones later on for visualization
 
 plants.x.BEC <- st_intersection(plants, BEC)
 
-# For some reason the polygon geometry from the BEC object is being replaced by point geometries from the records
-# Substitute the values from the BEC geometry field based on common MAP_LABEL field
+# Export catalog of records with BEC Zone attribute (discarding meaningless point geometries)
 
-plants.x.BEC$geometry  <- BEC$geometry[match(unlist(plants.x.BEC$MAP_LABEL), BEC$MAP_LABEL)]
+plants.x.BEC <- st_as_sf(plants.x.BEC) %>% st_drop_geometry()
+
+write.csv(plants.x.BEC, "../outputs/AHSBR_vascular_plant_records_x_BEC_Zones_2024.csv", row.names = FALSE)
 
 # Export CSVs: vascular plant records by BEC unit
 
 CMAunp.plants <- plants.x.BEC %>% filter(MAP_LABEL == 'CMAunp')
-CMAunp.plants$MAP_LABEL <- NULL
 
 CWHdm.plants <- plants.x.BEC %>% filter(MAP_LABEL == 'CWHdm')
-CWHdm.plants$MAP_LABEL <- NULL
 
 CWHds1.plants <- plants.x.BEC %>% filter(MAP_LABEL == 'CWHds1')
-CWHds1.plants$MAP_LABEL <- NULL
 
 CWHms1.plants <- plants.x.BEC %>% filter(MAP_LABEL == 'CWHms1')
-CWHms1.plants$MAP_LABEL <- NULL
 
 CWHvm1.plants <- plants.x.BEC %>% filter(MAP_LABEL == 'CWHvm1')
-CWHvm1.plants$MAP_LABEL <- NULL
 
 CWHvm2.plants <- plants.x.BEC %>% filter(MAP_LABEL == 'CWHvm2')
-CWHvm2.plants$MAP_LABEL <- NULL
 
 CWHxm1.plants <- plants.x.BEC %>% filter(MAP_LABEL == 'CWHxm1')
-CWHxm1.plants$MAP_LABEL <- NULL
 
 MHmm1.plants <- plants.x.BEC %>% filter(MAP_LABEL == 'MHmm1')
-MHmm1.plants$MAP_LABEL <- NULL
 
 MHmm2.plants <- plants.x.BEC %>% filter(MAP_LABEL == 'MHmm2')
-MHmm2.plants$MAP_LABEL <- NULL
 
 write.csv(CMAunp.plants, "../outputs/AHSBR_CMAunp_vascular_plants_2024.csv", row.names = FALSE)
 write.csv(CWHdm.plants, "../outputs/AHSBR_CWHdm_vascular_plants_2024.csv", row.names = FALSE)
@@ -126,10 +121,10 @@ write.csv(CWHxm1.plants, "../outputs/AHSBR_CWHxm1_vascular_plants_2024.csv", row
 write.csv(MHmm1.plants, "../outputs/AHSBR_MHmm1_vascular_plants_2024.csv", row.names = FALSE)
 write.csv(MHmm2.plants, "../outputs/AHSBR_MHmm2_vascular_plants_2024.csv", row.names = FALSE)
 
-# Reduce the dataframe based on distinct scientificName
+# Summarize by species and BEC Zone
 plants.x.BEC <- plants.x.BEC %>%
-  distinct(scientificName, .keep_all = TRUE)
+  distinct(scientificName, MAP_LABEL, .keep_all = TRUE)
 
-# Output plants.x.BEC for visualization
+# Output plants.x.BEC as a dataframe for visualization
 
-st_write(plants.x.BEC, "../outputs/BEC_x_plants_2024.shp")
+write.csv(plants.x.BEC, "../outputs/BEC_x_vascular_plants_summary_2024.csv", row.names = FALSE)
